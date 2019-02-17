1ba7c1237e5668c5f22e2c5aeb1c88851ff61e36
WISARUT CHALOEMANUKULWATTANA <wisarut@sec.or.th>
2019-02-17 21:31:16 +0700

    first commit

---
+++ ExampleRedis/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using ExampleRedis.Services;
+namespace ExampleRedis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            Console.WriteLine("Hello World!");
+            IRedisService redisService = new RedisService();
+            Console.Write("Connection : "+redisService.GetRedisClient());
+            Console.WriteLine("Save in memory database");
+            program.ReadData();
+            program.SaveData();
+            Console.ReadKey();
+        }
+        public void SaveData()
+        {
+            IRedisService redisService = new RedisService();
+            var cache = redisService.GetRedisClient();
+            for(int i=0;i<10;i++)
+            {
+                var value = cache.StringSet($"value : {0}",i);
+            }
+        }
+        public void ReadData()
+        {
+            IRedisService redisService = new RedisService();
+            var cache = redisService.GetRedisClient();
+            for (int i = 0; i < 10; i++)
+            {
+                var value = cache.StringGet($"value : {i}");
+                Console.WriteLine($"Value = {value}");
+            }
+        }
+    }
+}
---
+++ ExampleRedis/Services/RedisService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StackExchange.Redis;
+
+namespace ExampleRedis.Services
+{
+    class RedisService : IRedisService
+    {
+        readonly IDatabase _redisClient;
+
+        public RedisService()
+        {
+            ConnectionMultiplexer connection = ConnectionMultiplexer.Connect("localhost:6379");
+            IDatabase database = connection.GetDatabase();
+            _redisClient = database;
+        }
+        public IDatabase GetRedisClient()
+        {
+            return _redisClient;
+        }
+    }
+}
